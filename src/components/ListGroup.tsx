602db30952a05384eb00b310f228d3fd543044ac
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-01-23 23:44:48 +0100

    updating

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["new york", "los angeles", "chicago", "houston", "phoenix"];
+interface Props {
+    items: string[];
+    heading: string;
+    onSelectItem: (item: string) => void;
+}
 
+function ListGroup({ items, heading }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       <ul className="list-group">
         {items.map((item, index) => (
           <li
