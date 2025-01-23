7fc699ca6e14bca23d031061f0e080e875cd019a
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-01-23 23:51:57 +0100

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <ListGroup />;
+  const items = ["new york", "los angeles", "chicago", "houston", "phoenix"];
+  function onSelectItemHandler() {
+    console.log("Item selected");
+  }
+  return (
+    <ListGroup
+      heading="Cities"
+      items={items}
+      onSelectItem={onSelectItemHandler}
+    />
+  );
 }
 
 export default App;
